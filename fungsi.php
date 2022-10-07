308cea5b008621b0adec1aadd3eddc9abbac8d07
fachriananda <107461128+fachriananda@users.noreply.github.com>
2022-10-07 15:50:18 +0700

    file config
    file config

---
+++ fungsi.php
@@ -0,0 +1,20 @@
+<?php
+include 'koneksi.php';
+
+function getValue($field,$table,$term){
+	$z = mysql_fetch_array(mysql_query("SELECT $field FROM $table WHERE $term"));
+	return $z[0];
+}
+
+function getNumRows($query){
+	$z = mysql_num_rows(mysql_query("$query"));
+	return $z;
+}
+/*
+function getadmin($_FILES){
+
+$z =  mysql_fetch_array(mysql_query("SELECT $_FILES"));
+return $z[1];
+}*/
+
+?>
